750eb87969ac3f599b964bd7f73c36225c508bf0
JaganKaartik <kaartikjagan@gmail.com>
2017-01-17 10:37:19 -0800

    Added water.c

---
+++ water.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int mins,litrs;
+ printf("Enter no of Minutes : ");
+ scanf("%d",mins);
+ litrs=mins*12;
+ printf("Litres : ");
+ scanf("%d",litrs);
+ return 0;
+}

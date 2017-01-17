fb227e3ffa44ef5b40b2009ee6c1505c9bea1613
JaganKaartik <kaartikjagan@gmail.com>
2017-01-17 10:18:12 -0800

    Added caesar.cpp

---
+++ caesar.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
+#include<ctype.h>
+
+using namespace std;
+
+
+int main()
+{
+	char str[10];
+	
+	int n,k;
+	
+	cout<<"Enter a String : ";
+	cin.getline(str,10);
+	cout<<"Enter the key : ";
+	cin>>k;
+	
+	n=strlen(str);
+	
+    for(int i=0;i<n;++i)
+	{
+		if (isalpha(str[i]))
+		{
+         str[i] = toupper(str[i]); 
+         str[i] = (((str[i]-65) + k) % 26) + 65;
+        }
+    
+	}
+	
+	cout<<"Cipher Text : "<<str<<endl;
+	return 0;	
+}
+
